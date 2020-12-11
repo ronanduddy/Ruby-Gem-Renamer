d8e043de24c60d80cc6770a1e7060a22c34da867
ronanduddy <ronanduddy@live.ie>
2020-12-11 10:09:15 +0000

    Attempt fix for gem not running correctly when installed
    I get the following:
    ```
    /usr/src/app # gem install rename_gem
    Fetching rename_gem-0.0.3.gem
    Successfully installed rename_gem-0.0.3
    1 gem installed
    /usr/src/app # rename_gem
    Traceback (most recent call last):
    	7: from /usr/local/bundle/bin/rename_gem:23:in `<main>'
    	6: from /usr/local/bundle/bin/rename_gem:23:in `load'
    	5: from /usr/local/bundle/gems/rename_gem-0.0.3/bin/rename_gem:4:in `<top (required)>'
    	4: from /usr/local/lib/ruby/2.7.0/rubygems/core_ext/kernel_require.rb:92:in `require'
    	3: from /usr/local/lib/ruby/2.7.0/rubygems/core_ext/kernel_require.rb:92:in `require'
    	2: from /usr/local/bundle/gems/rename_gem-0.0.3/lib/rename_gem.rb:3:in `<top (required)>'
    	1: from /usr/local/lib/ruby/2.7.0/rubygems/core_ext/kernel_require.rb:92:in `require'
    /usr/local/lib/ruby/2.7.0/rubygems/core_ext/kernel_require.rb:92:in `require': cannot load such file -- rename_gem/cli (LoadError)
    
    ```

--- lib/rename_gem/renamer.rb
+++ lib/rename_gem/renamer.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-module RenameGem
-  require 'rename_gem/renamer/runner'
-  require 'rename_gem/renamer/entity'
-  require 'rename_gem/renamer/path'
-  require 'rename_gem/renamer/file_handler'
-  require 'rename_gem/renamer/directory_handler'
-  require 'rename_gem/renamer/modifier'
-  require 'rename_gem/renamer/possession'
-  require 'rename_gem/renamer/string_replacer'
+require 'rename_gem/renamer/runner'
+require 'rename_gem/renamer/entity'
+require 'rename_gem/renamer/path'
+require 'rename_gem/renamer/file_handler'
+require 'rename_gem/renamer/directory_handler'
+require 'rename_gem/renamer/modifier'
+require 'rename_gem/renamer/possession'
+require 'rename_gem/renamer/string_replacer'
 
+module RenameGem
   module Renamer
     def self.run(options)
       path = Pathname.new(Dir.pwd).join(options[:path]).to_s
--- lib/rename_gem/version.rb
+++ lib/rename_gem/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RenameGem
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
