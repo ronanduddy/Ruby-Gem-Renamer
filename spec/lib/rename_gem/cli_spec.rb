8b255a40c3d5f8756f4ad620c27f6bb04d4eb91a
ronanduddy <ronanduddy@live.ie>
2020-12-15 18:05:27 +0000

    Create acceptance-based test covering correct output

--- spec/lib/rename_gem/cli_spec.rb
+++ spec/lib/rename_gem/cli_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'support/helpers/fake_file_system_helpers'
+
 RSpec.describe CLI do
   let(:cli) { described_class.new }
   let(:remamer) { Renamer }
@@ -24,5 +26,36 @@ RSpec.describe CLI do
         rename
       end
     end
+  end
+
+  describe '.start', :fakefs do
+    subject(:start) { RenameGem::CLI.start(argv) }
+    let(:argv) {["rename", "-f", from, "-t", to, "-p", path]}
+    let(:from) { 'project_name' }
+    let(:to) { 'foo_bar'}
+    let(:path) { test_gem_fixtures_dir }
+
+    before { activate_fakefs(test_gem_fixtures_dir) }
+    after { deactivate_fakefs }
+
+    let(:results) do
+      <<~STR
+        Edit /usr/src/app/spec/fixtures/test_gem/project_name.gemspec
+        Rename /usr/src/app/spec/fixtures/test_gem/project_name.gemspec -> foo_bar.gemspec
+        Edit /usr/src/app/spec/fixtures/test_gem/README.md
+        Edit /usr/src/app/spec/fixtures/test_gem/docker-compose.yml
+        Edit /usr/src/app/spec/fixtures/test_gem/bin/console
+        Edit /usr/src/app/spec/fixtures/test_gem/exe/project_name
+        Rename /usr/src/app/spec/fixtures/test_gem/exe/project_name -> foo_bar
+        Edit /usr/src/app/spec/fixtures/test_gem/lib/project_name.rb
+        Rename /usr/src/app/spec/fixtures/test_gem/lib/project_name.rb -> foo_bar.rb
+        Rename /usr/src/app/spec/fixtures/test_gem/lib/project_name -> foo_bar
+        Edit /usr/src/app/spec/fixtures/test_gem/lib/foo_bar/version.rb
+        Edit /usr/src/app/spec/fixtures/test_gem/spec/spec_helper.rb
+        Rename /usr/src/app/spec/fixtures/test_gem/spec/support/lib/project_name -> foo_bar
+      STR
+    end
+
+    specify { expect { start }.to output(results).to_stdout }
   end
 end
